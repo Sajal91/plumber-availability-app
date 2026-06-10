71b59eb1cfddadc54444d795676b6fd91644fa09
Sajal91 <142242683+Sajal91@users.noreply.github.com>
2026-06-11 01:56:46 +0530

    Delete src directory

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-const API_END_POINT = process.env.API_END_POINT;
-const PORT = 5000;
-
-export const API_BASE_URL = `http://${API_END_POINT}:${PORT}/api`;
-export const SOCKET_URL = `http://${API_END_POINT}:${PORT}`;
